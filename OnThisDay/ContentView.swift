020b32523b7204cbc9ee261de39df3ad7494e1f1
Zach Howes <zhowes14@icloud.com>
2022-06-07 23:36:17 -0400

    Initial Commit

---
+++ OnThisDay/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OnThisDay
+//
+//  Created by Zach Howes on 6/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ OnThisDay/OnThisDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnThisDayApp.swift
+//  OnThisDay
+//
+//  Created by Zach Howes on 6/7/22.
+//
+
+import SwiftUI
+
+@main
+struct OnThisDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
